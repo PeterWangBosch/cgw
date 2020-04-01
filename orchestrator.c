2e267305b9553df0be46199b9924362db597e959
Peter Wang <Peter.WANG3@cn.bosch.com>
2020-04-01 09:18:22 +0800

    Support upgrading multi-devices

--- orchestrator.c
+++ orchestrator.c
@@ -4,6 +4,7 @@
  */
 
 #include "stdio.h"
+#include "cJSON/cJSON.h"
 #include "src/mongoose.h"
 
 //------------------------------------------------------------------
@@ -17,28 +18,66 @@ struct bs_pkg_info {
   char release_notes[256];
 };
 
-/**
- * Upgrading status of single ECU
-**/
-struct bs_ecu_upgrade_stat {
-  char esti_time[64];
+struct bs_app_upgrade_stat {
+  char esti_time[32];
   char start_time[32];
   char time_stamp[32];
-  // "yes" or "no"
-  char door_module[8];
-  // "pending", "in progress", "failed", "success"
+  // deifferent app might have differnt status set 
   char status[16];
   // raw percentage data, e.g., 0, 55, or 100
   float progress_percent;
 };
 
-struct bs_context {
+#define BS_PKG_TYPE_INVALID 0
+#define BS_PKG_TYPE_CAN_ECU 1
+#define BS_PKG_TYPE_ETH_ECU 2
+#define BS_PKG_TYPE_ORCH 3
+const char *bs_pkg_stat_idle = "idle";
+const char *bs_pkg_stat_loading = "loading";
+const char *bs_pkg_stat_succ = "succ";
+const char *bs_pkg_stat_fail = "fail";
+struct bs_app_pkg_stat {
+  unsigned char type;
+  const char *stat;
+};
+
+struct bs_app_intaller_proxy {
+  unsigned int ip_addr;
+  unsigned int port;
+  void *thread_id;
+  bool thread_exit;
+  struct mg_connection *remote;
+};
+
+struct bs_device_app {
   char dev_id[32];
   char soft_id[32];
-  unsigned int door_module;
-  char* pkg_stat; // "no start", "downloading", "succ", "fail"
-  struct bs_ecu_upgrade_stat upgrade_stat;
+  bool door_module; 
+  struct bs_app_pkg_stat pkg_stat;
+  struct bs_app_upgrade_stat upgrade_stat;
+  struct bs_app_intaller_proxy installer;
 };
+
+#define BS_MAX_DEVICE_APP_NUM 128
+struct bs_context {
+  struct mg_connection * tlc;
+  struct bs_device_app *loading_app;// TODO: support downloading in paralell
+  struct bs_device_app apps[BS_MAX_DEVICE_APP_NUM];
+};
+
+//-----------------------------------------------------------------------
+// Protocol JSON parser
+//-----------------------------------------------------------------------
+const char * api_resp_pkg_proto_http = "http";//TODO: in future, supports ftps, tftp
+const char * api_resp_err_succ = "succ";
+const char * api_resp_err_devid = "err devid";
+const char * api_resp_err_payload = "err payload";
+const char * api_resp_err_fail = "fail"; 
+
+#define JSON_INIT 0
+#define JSON_HAS_DEVID 1
+#define JSON_HAS_PAYLOAD 2
+typedef int (* cJSONHookFn) (const cJSON *);
 
 //------------------------------------------------------------------
 // Global
@@ -100,10 +139,73 @@ static int on_write_data(FILE *fp, uint8_t *buffer, int len)
     return 1;
 }
 
+//------------------------------------------------------------------
+// Handle core data
+//------------------------------------------------------------------
+struct bs_device_app * core_find_app(struct bs_context *p_ctx, const char *id)
+{
+  struct bs_device_app * result = NULL;
+  int i;
+
+  for (i=0; i<BS_MAX_DEVICE_APP_NUM; i++) {
+    if (strcmp(p_ctx->apps[i].dev_id, id) == 0) {
+      result = &(p_ctx->apps[i]);
+      return result;
+    }
+  }
+  return result;
+}
+
+int core_api_new_pkg(struct bs_device_app *app, struct cJSON *payload)
+{
+  int result = 1;
+
+  switch(app->pkg_stat.type) {
+    case BS_PKG_TYPE_CAN_ECU:
+    case BS_PKG_TYPE_ORCH:
+      // local download & cache
+      app->pkg_stat.stat = bs_pkg_stat_loading;
+      break;
+    case BS_PKG_TYPE_ETH_ECU:
+      //TODO: start remote installer job
+      (void) payload;
+      break;
+    case BS_PKG_TYPE_INVALID:
+    default:
+      result = 0;
+      break;
+  }  
+
+  return result;
+}
+
+int core_api_tdr_run(struct bs_device_app *app, struct cJSON *payload)
+{
+  int result = 1;
+
+  switch(app->pkg_stat.type) {
+    case BS_PKG_TYPE_CAN_ECU:
+    case BS_PKG_TYPE_ORCH:
+      // local download & cache
+      app->pkg_stat.stat = bs_pkg_stat_loading;
+      break;
+    case BS_PKG_TYPE_ETH_ECU:
+      //TODO: start remote installer job
+      (void) payload;
+      break;
+    case BS_PKG_TYPE_INVALID:
+    default:
+      result = 0;
+      break;
+  }  
+
+  return result;
+}
 
 //------------------------------------------------------------------
 // HTTP Message handler
 //------------------------------------------------------------------
+
 // for live testing
 static void handle_test_live(struct mg_connection *nc, int ev, void *p) {
   (void) ev;
@@ -116,15 +218,62 @@ static void handle_test_live(struct mg_connection *nc, int ev, void *p) {
 }
 
 static void handle_pkg_new(struct mg_connection *nc, int ev, void *p) {
+  struct http_message *hm = (struct http_message *) p;
+  const char *result = api_resp_err_succ;
+  const char *proto = api_resp_pkg_proto_http;
+  struct bs_device_app *app; 
+
+  int parse_stat = JSON_INIT;
+  //static char response[1024];
+  struct cJSON * root = NULL;
+  struct cJSON * iterator = NULL;
+  struct cJSON * payload = NULL;
+  char * dev_id = NULL;
+
   (void) ev;
-  (void) p;
+  printf("Raw msg from TLC: %s\n", hm->body.p);
+  if (!hm->body.p)
+    return;
 
+  root = cJSON_Parse(hm->body.p);
+  //TODO: check code of cJSON if memory leave when root is NULL
+  if (root == NULL) {
+    result = api_resp_err_fail;
+    goto last_step; 
+  }
+
+  iterator = root->child;
+  while(iterator) {
+    if (strcmp(iterator->string, "deviceId") == 0) {
+      parse_stat = JSON_HAS_DEVID;
+      dev_id = iterator->valuestring;
+    }
+  }
+  if (parse_stat != JSON_HAS_DEVID) {
+    result = api_resp_err_devid;
+    goto last_step;
+  } 
+
+  payload = cJSON_GetObjectItem(root, "payload");
+  if (!payload) {
+    result = api_resp_err_payload;
+    goto last_step; 
+  }
+  app = core_find_app(&g_ctx, dev_id);
+  if (core_api_new_pkg(app, payload)) {
+    g_ctx.loading_app = app;
+  } else {
+    result = api_resp_err_fail;
+    goto last_step;
+  }
+
+last_step:
   // TODO: configuable to tftp, https
-  char * proto = "http";
-
   mg_printf(nc, "%s", "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
-  mg_printf_http_chunk(nc, "{ \"protocol\": %s }", proto);
+  mg_printf_http_chunk(nc, "{ \"protocol\": %s, \"error\": %s }", proto, result);
   mg_send_http_chunk(nc, "", 0); /* Send empty chunk, the end of response */
+  // release memory
+  cJSON_Delete(root);
 }
 
 
@@ -133,7 +282,7 @@ static void handle_pkg_stat(struct mg_connection *nc, int ev, void *p) {
   (void) p;
 
   mg_printf(nc, "%s", "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
-  mg_printf_http_chunk(nc, "{ \"result\": %s }", g_ctx.pkg_stat);
+  mg_printf_http_chunk(nc, "{ \"result\": %s }", "succ");//g_ctx.pkg_stat);
   mg_send_http_chunk(nc, "", 0); /* Send empty chunk, the end of response */
 }
 
@@ -203,7 +352,7 @@ static void handle_upload(struct mg_connection *nc, int ev, void *p) {
           return;
         }
         nc->user_data = (void *) data;
-        g_ctx.pkg_stat = "downloading";
+        //g_ctx.pkg_stat = "downloading";
       }
       break;
     }
@@ -230,7 +379,10 @@ static void handle_upload(struct mg_connection *nc, int ev, void *p) {
 
       nc->flags |= MG_F_SEND_AND_CLOSE;
       fclose(data->fp);
-      g_ctx.pkg_stat = "succ";
+
+      g_ctx.loading_app->pkg_stat.stat = bs_pkg_stat_succ;
+      g_ctx.loading_app = NULL;
+
       free(data);
       nc->user_data = NULL;
       break;
@@ -247,8 +399,39 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
 //------------------------------------------------------------------
 // HTTP Thread
 //------------------------------------------------------------------
-void init_context(){
-  g_ctx.pkg_stat = "no start";
+void init_device_app(struct bs_device_app *app) {
+  int i;
+
+  for (i=0; i<32; i++) {
+    app->dev_id[0] = 0;
+    app->soft_id[0] = 0;
+  }
+  app->door_module = false;; 
+
+  app->pkg_stat.type = BS_PKG_TYPE_INVALID;
+  app->pkg_stat.stat = bs_pkg_stat_succ;
+
+  strcpy(app->upgrade_stat.esti_time, "00:00:00 1900-01-01");
+  strcpy(app->upgrade_stat.start_time, "00:00:00 1900-01-01");
+  strcpy(app->upgrade_stat.time_stamp, "00:00:00 1900-01-01");
+  for (i=0; i<16; i++) {
+    app->upgrade_stat.status[i] = 0;
+  }
+  app->upgrade_stat.progress_percent = 0;
+
+  app->installer.ip_addr = 0;
+  app->installer.port = 0;
+  app->installer.thread_id = NULL;
+  app->installer.thread_exit = false;
+}
+
+void init_context() {
+  int i;
+
+  g_ctx.tlc = NULL;
+  for(i=0; i<BS_MAX_DEVICE_APP_NUM; i++) {
+    init_device_app(g_ctx.apps + i);
+  }
 }
 
 int main(int argc, char *argv[]) {
